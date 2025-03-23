417eaa43d29bd85854ca1cc44b13c65b7734fbc5
William <96638909+WilliamCallao@users.noreply.github.com>
2025-03-23 23:14:56 +0100

    Update

--- app/role-student/problems/editor-component.jsx
+++ app/role-student/problems/editor-component.jsx
@@ -1,7 +1,7 @@
 export default function EditorComponent() {
   return (
     <div className="flex h-full w-full items-center justify-center">
-      <div className="text-2xl font-bold text-white">Editor</div>
+      <div className="text-2xl font-bold text-white">A</div>
     </div>
   )
 }
--- app/role-student/problems/resources-component.jsx
+++ app/role-student/problems/resources-component.jsx
@@ -1,7 +1,7 @@
 export default function ResourcesComponent() {
   return (
     <div className="flex h-full w-full items-center justify-center bg-green-900">
-      <div className="text-2xl font-bold text-white">Recursos</div>
+      <div className="text-2xl font-bold text-white">B</div>
     </div>
   )
 }
