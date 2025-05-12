227d3c5a840b462d2111b79436fde475c50701dc
Adrian <adrianpereztapia@gmail.com>
2025-05-12 01:56:04 -0400

    feat: añadir ComponentMemorySimulator como wrapper de integración

---
+++ application/app/role-admin/enrollment/ComponentMemorySimulator.js
@@ -0,0 +1,12 @@
+//Componente wrapper de integración
+"use client"
+
+import MemorySimulator from "@/components/MemorySimulator"
+
+export default function ComponentMemorySimulator() {
+  return (
+    <div className="w-full">
+      <MemorySimulator />
+    </div>
+  )
+}
